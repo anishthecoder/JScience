9fcabb6a991c88f3e45dc53fa4f86db6ddcda7e9
Anish Abraham <anish@wardoberlab.com>
2015-10-09 19:53:37 -0500

    Additional kryo corrections.

--- src/javax/measure/converter/AddConverter.java
+++ src/javax/measure/converter/AddConverter.java
@@ -1,80 +1,92 @@
 /*
  * JScience - Java(TM) Tools and Libraries for the Advancement of Sciences.
- * Copyright (C) 2006 - JScience (http://jscience.org/)
- * All rights reserved.
+ * Copyright (C) 2006 - JScience (http://jscience.org/) All rights reserved.
  * 
- * Permission to use, copy, modify, and distribute this software is
- * freely granted, provided that this notice is preserved.
+ * Permission to use, copy, modify, and distribute this software is freely
+ * granted, provided that this notice is preserved.
  */
 package javax.measure.converter;
 
-
 /**
- * <p> This class represents a converter adding a constant offset 
- *     (approximated as a <code>double</code>) to numeric values.</p>
- *     
- * <p> Instances of this class are immutable.</p>
+ * <p>
+ * This class represents a converter adding a constant offset (approximated as a
+ * <code>double</code>) to numeric values.
+ * </p>
+ * 
+ * <p>
+ * Instances of this class are immutable.
+ * </p>
  *
- * @author  <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
+ * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @version 3.1, April 22, 2006
  */
 public final class AddConverter extends UnitConverter {
 
-    /**
-     * Holds the offset.
-     */
-    private final double _offset;
+	/**
+	 * Holds the offset.
+	 */
+	private final double _offset;
 
-    /**
-     * Creates an add converter with the specified offset.
-     *
-     * @param  offset the offset value.
-     * @throws IllegalArgumentException if offset is zero (or close to zero).
-     */
-    public AddConverter(double offset) {
-        if ((float)offset == 0.0)
-            throw new IllegalArgumentException("Identity converter not allowed");
-        _offset = offset;
-    }
+	/* For serializers. */
+	@SuppressWarnings("unused")
+	private AddConverter() {
+		_offset = Double.NaN;
+	}
 
-    /**
-     * Returns the offset value for this add converter.
-     *
-     * @return the offset value.
-     */
-    public double getOffset() {
-        return _offset;
-    }
-    
-    @Override
-    public UnitConverter inverse() {
-        return new AddConverter(- _offset);
-    }
+	/**
+	 * Creates an add converter with the specified offset.
+	 *
+	 * @param offset
+	 *        the offset value.
+	 * @throws IllegalArgumentException
+	 *         if offset is zero (or close to zero).
+	 */
+	public AddConverter(final double offset) {
+		if ((float) offset == 0.0)
+			throw new IllegalArgumentException("Identity converter not allowed");
+		_offset = offset;
+	}
 
-    @Override
-    public double convert(double amount) {
-        return amount + _offset;
-    }
+	/**
+	 * Returns the offset value for this add converter.
+	 *
+	 * @return the offset value.
+	 */
+	public double getOffset() {
+		return _offset;
+	}
 
-    @Override
-    public boolean isLinear() {
-        return false;
-    }
+	@Override
+	public UnitConverter inverse() {
+		return new AddConverter(-_offset);
+	}
 
-    @Override
-    public UnitConverter concatenate(UnitConverter converter) {
-        if (converter instanceof AddConverter) {
-            double offset = _offset + ((AddConverter)converter)._offset;
-            return valueOf(offset);
-        } else {
-            return super.concatenate(converter);
-        }
-    }
+	@Override
+	public double convert(final double amount) {
+		return amount + _offset;
+	}
 
-    private static UnitConverter valueOf(double offset) {
-        float asFloat = (float) offset;
-        return asFloat == 0.0f ? UnitConverter.IDENTITY : new AddConverter(offset);
-    }
-    
-    private static final long serialVersionUID = 1L;
+	@Override
+	public boolean isLinear() {
+		return false;
+	}
+
+	@Override
+	public UnitConverter concatenate(final UnitConverter converter) {
+		if (converter instanceof AddConverter) {
+			final double offset = _offset + ((AddConverter) converter)._offset;
+			return valueOf(offset);
+		}
+		else {
+			return super.concatenate(converter);
+		}
+	}
+
+	private static UnitConverter valueOf(final double offset) {
+		final float asFloat = (float) offset;
+		return asFloat == 0.0f ? UnitConverter.IDENTITY : new AddConverter(
+				offset);
+	}
+
+	private static final long serialVersionUID = 1L;
 }
--- src/javax/measure/unit/CompoundUnit.java
+++ src/javax/measure/unit/CompoundUnit.java
@@ -1,10 +1,9 @@
 /*
  * JScience - Java(TM) Tools and Libraries for the Advancement of Sciences.
- * Copyright (C) 2006 - JScience (http://jscience.org/)
- * All rights reserved.
+ * Copyright (C) 2006 - JScience (http://jscience.org/) All rights reserved.
  * 
- * Permission to use, copy, modify, and distribute this software is
- * freely granted, provided that this notice is preserved.
+ * Permission to use, copy, modify, and distribute this software is freely
+ * granted, provided that this notice is preserved.
  */
 package javax.measure.unit;
 
@@ -12,98 +11,112 @@ import javax.measure.converter.UnitConverter;
 import javax.measure.quantity.Quantity;
 
 /**
- * <p> This class represents the multi-radix units (such as "hour:min:sec"). 
- *     Instances of this class are created using the {@link Unit#compound
- *     Unit.compound} method.</p>
- *      
- * <p> Examples of compound units:[code]
- *     Unit<Duration> HOUR_MINUTE_SECOND = HOUR.compound(MINUTE).compound(SECOND);
- *     Unit<Angle> DEGREE_MINUTE_ANGLE = DEGREE_ANGLE.compound(MINUTE_ANGLE);
- *     [/code]</p>
+ * <p>
+ * This class represents the multi-radix units (such as "hour:min:sec").
+ * Instances of this class are created using the {@link Unit#compound
+ * Unit.compound} method.
+ * </p>
+ * 
+ * <p>
+ * Examples of compound units:[code] Unit<Duration> HOUR_MINUTE_SECOND =
+ * HOUR.compound(MINUTE).compound(SECOND); Unit<Angle> DEGREE_MINUTE_ANGLE =
+ * DEGREE_ANGLE.compound(MINUTE_ANGLE); [/code]
+ * </p>
  *
- * @author  <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
+ * @author <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
  * @version 3.1, April 22, 2006
  */
 public final class CompoundUnit<Q extends Quantity> extends DerivedUnit<Q> {
 
-    /**
-     * Holds the higher unit.
-     */
-    private final Unit<Q> _high;
+	/**
+	 * Holds the higher unit.
+	 */
+	private final Unit<Q> _high;
 
-    /**
-     * Holds the lower unit.
-     */
-    private final Unit<Q> _low;
+	/**
+	 * Holds the lower unit.
+	 */
+	private final Unit<Q> _low;
 
-    /**
-     * Creates a compound unit from the specified units. 
-     *
-     * @param  high the high unit.
-     * @param  low the lower unit(s)
-     * @throws IllegalArgumentException if both units do not the same system
-     *         unit.
-     */
-    CompoundUnit(Unit<Q> high, Unit<Q> low) {
-        if (!high.getStandardUnit().equals(low.getStandardUnit()))
-            throw new IllegalArgumentException(
-                    "Both units do not have the same system unit");
-        _high = high;
-        _low = low;
-        
-    }
+	/* For serializers. */
+	@SuppressWarnings("unused")
+	private CompoundUnit() {
+		_high = null;
+		_low = null;
+	}
 
-    /**
-     * Returns the lower unit of this compound unit.
-     *
-     * @return the lower unit.
-     */
-    public Unit<Q> getLower() {
-        return _low;
-    }
+	/**
+	 * Creates a compound unit from the specified units.
+	 *
+	 * @param high
+	 *        the high unit.
+	 * @param low
+	 *        the lower unit(s)
+	 * @throws IllegalArgumentException
+	 *         if both units do not the same system unit.
+	 */
+	CompoundUnit(final Unit<Q> high, final Unit<Q> low) {
+		if (!high.getStandardUnit().equals(low.getStandardUnit()))
+			throw new IllegalArgumentException(
+					"Both units do not have the same system unit");
+		_high = high;
+		_low = low;
 
-    /**
-     * Returns the higher unit of this compound unit.
-     *
-     * @return the higher unit.
-     */
-    public Unit<Q> getHigher() {
-        return _high;
-    }
+	}
 
-    /**
-     * Indicates if this compound unit is considered equals to the specified 
-     * object (both are compound units with same composing units in the 
-     * same order).
-     *
-     * @param  that the object to compare for equality.
-     * @return <code>true</code> if <code>this</code> and <code>that</code>
-     *         are considered equals; <code>false</code>otherwise. 
-     */
-    public boolean equals(Object that) {
-        if (this == that)
-            return true;
-        if (!(that instanceof CompoundUnit))
-            return false;
-        CompoundUnit<?> thatUnit = (CompoundUnit<?>) that;
-        return this._high.equals(thatUnit._high)
-                && this._low.equals(thatUnit._low);
-    }
+	/**
+	 * Returns the lower unit of this compound unit.
+	 *
+	 * @return the lower unit.
+	 */
+	public Unit<Q> getLower() {
+		return _low;
+	}
 
-    @Override
-    public int hashCode() {
-        return _high.hashCode() ^ _low.hashCode();
-    }
+	/**
+	 * Returns the higher unit of this compound unit.
+	 *
+	 * @return the higher unit.
+	 */
+	public Unit<Q> getHigher() {
+		return _high;
+	}
 
-    @Override
-    public Unit<? super Q> getStandardUnit() {
-        return _low.getStandardUnit(); 
-    }
+	/**
+	 * Indicates if this compound unit is considered equals to the specified
+	 * object (both are compound units with same composing units in the same
+	 * order).
+	 *
+	 * @param that
+	 *        the object to compare for equality.
+	 * @return <code>true</code> if <code>this</code> and <code>that</code> are
+	 *         considered equals; <code>false</code>otherwise.
+	 */
+	@Override
+	public boolean equals(final Object that) {
+		if (this == that)
+			return true;
+		if (!(that instanceof CompoundUnit))
+			return false;
+		final CompoundUnit<?> thatUnit = (CompoundUnit<?>) that;
+		return this._high.equals(thatUnit._high)
+				&& this._low.equals(thatUnit._low);
+	}
 
-    @Override
-    public UnitConverter toStandardUnit() {
-        return _low.toStandardUnit();
-    }
+	@Override
+	public int hashCode() {
+		return _high.hashCode() ^ _low.hashCode();
+	}
 
-    private static final long serialVersionUID = 1L;
+	@Override
+	public Unit<? super Q> getStandardUnit() {
+		return _low.getStandardUnit();
+	}
+
+	@Override
+	public UnitConverter toStandardUnit() {
+		return _low.toStandardUnit();
+	}
+
+	private static final long serialVersionUID = 1L;
 }
